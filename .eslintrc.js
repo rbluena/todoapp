e39dac20f310dddbfb02a0628a04c62a501ba792
Rabii Luena <itisrabii@gmail.com>
2022-11-04 10:27:24 +0300

    linter configurations

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+		jest: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:react-hooks/recommended',
+		'prettier',
+		'next',
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/no-explicit-any': 'warn',
+	},
+};
